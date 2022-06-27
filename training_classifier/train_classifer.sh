9137908267c67fef78109ed7ebf982d88cce91de
Browne1997 <91316035+Browne1997@users.noreply.github.com>
2022-06-27 12:01:15 +0100

    Update train_classifer.sh

--- training_classifier/train_classifer.sh
+++ training_classifier/train_classifer.sh
@@ -3,3 +3,14 @@
 
 # Notes:
 # -map: provides calculated mean Average Precision scores and average CIoU loss
+# -thresh: sets the average CIoU threshold
+
+# 1. Open bash shell terminal window
+# 2. cd to the CWD with the file structure seen in diagram 2.2
+# 3. run following bash script line 
+
+# here you must edit the X's to a value of choice, insert name of classifier for both the .cfg (configuration file) and .conv.137 (weights file)
+./darknet detector train data/obj.data ./classifier.cfg ./classifier.conv.137 -map X -thresh X
+
+# OR download this whole script, put in same CWD and run:
+bash train_classifier.sh
