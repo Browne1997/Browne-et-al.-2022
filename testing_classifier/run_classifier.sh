5209ac283563cbbabb6cef18886fa6f4731d432a
Browne1997 <91316035+Browne1997@users.noreply.github.com>
2022-06-27 12:07:54 +0100

    Update run_classifier.sh

--- testing_classifier/run_classifier.sh
+++ testing_classifier/run_classifier.sh
@@ -7,6 +7,8 @@
 # -thresh: CIoU threshold (i.e. confidence = 0.5 default)
 # Comment out livestream or video line depending on usage
 
+# Run the following code in a bash shell terminal from the directory the darknet.exe is found
+
 # Synthetic ship: run classifier on video file
 ./darknet detector demo ./deep_training/X.data ./deep_training/X.cfg ./deep_training/models/X.weights '/video/directory.mov' -thresh 0.5 -prefix /location/for/framegrabs/name_ -json_port 6666
 # In-situ ship: run classifier on livestream
